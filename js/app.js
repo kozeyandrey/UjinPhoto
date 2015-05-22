1a242591de978b5f34b2cc3044b5f31a293f234c
kikill95 <kirill.gus@gmail.com>
2015-05-22 15:01:45 +0300

    Adding actions on changing content

--- js/app.js
+++ js/app.js
@@ -1,5 +1,30 @@
 (function(){
+    $('header ul.nav').on('click', function(e) {
+        var $target = (e.target),
+            oldNumber = $('.content ul li.display').attr('class').charAt(4),
+            newNumber = $target.className[5];
 
+        if (oldNumber !== newNumber) {
+            $('#nav-page' + oldNumber)
+                .removeClass('display')
+                .addClass('no-display');
+            $('#nav-page' + newNumber)
+                .removeClass('no-display')
+                .addClass('display');
+
+            $('.photo' + oldNumber)
+                .removeClass('photo' + oldNumber + '-active');
+            $('.photo' + newNumber)
+                .addClass('photo' + newNumber + '-active');
+
+            $('#page' + oldNumber)
+                .removeClass('display')
+                .addClass('no-display');
+            $('#page' + newNumber)
+                .removeClass('no-display')
+                .addClass('display');
+        }
+    });
 
 })();
 
