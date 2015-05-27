79998db8e3a43c14686c0d01db93da39fb21aab5
VitalikDrobot <VitalikDrobot@ukr.net>
2015-05-27 19:04:54 +0300

    Fixe bag in styles

---
+++ js/app.js
@@ -0,0 +1,55 @@
+(function(){
+
+    $('header ul.nav').on('click', function(e) {
+        var $target = $(e.target),
+            oldNumber = $('.content ul li.display')[0].className[4],
+            newNumber = $target[0].className[5];
+
+        if (oldNumber !== newNumber) {
+            $('#nav-page' + oldNumber)
+                .removeClass('display')
+                .addClass('no-display');
+            $('#nav-page' + newNumber)
+                .removeClass('no-display')
+                .addClass('display');
+
+            $('.photo' + oldNumber)
+                .removeClass('photo' + oldNumber + '-active');
+            $('.photo' + newNumber)
+                .addClass('photo' + newNumber + '-active');
+
+            $('#page' + oldNumber)
+                .removeClass('display')
+                .addClass('no-display');
+            $('#page' + newNumber)
+                .removeClass('no-display')
+                .addClass('display');
+        }
+    });
+
+
+    $('#ca-container').contentcarousel();
+
+
+    $( "#tabs" ).tabs();
+
+
+    $('.content ul.book-list ul img').on('click', function(e) {
+        var $el = $(e.target).parent().parent().parent().find('img').first();
+        $el.animate({
+            opacity: 0
+        }, 200, function() {
+            $el[0].src = $(e.target)[0].src;
+            $el.animate({
+                opacity: 1
+            }, 200);
+        });
+    });
+
+})();
+
+function linkTo(rel) {
+    $('html, body').animate({
+        scrollTop: $('.' + rel).offset().top
+    }, 1000);
+}
