bc027ab31cb8c8bfa4b7e7376711d127857dac66
Ivancha-Igor <ivan4afamily@gmail.com>
2015-07-14 15:42:42 +0300

    Change in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -89,11 +89,11 @@ Rails.application.configure do
   config.action_mailer.raise_delivery_errors = false
   config.action_mailer.default :charset => "utf-8"
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = {
-      :address              => "smtp.gmail.com",
+  ActionMailer::Base.smtp_settings = {
+      :address              => 'smtp.sendgrid.net',
       :port                 => 587,
-      :user_name            => ENV['GMAIL_USERNAME'],
-      :password             => ENV['GMAIL_PASSWORD'],
+      :user_name            => ENV['SENDGRID_USERNAME'],
+      :password             => ENV['SENDGRID_PASSWORD'],
       :domain               => 'heroku.com',
       :authentication       => "plain",
       :enable_starttls_auto => true
